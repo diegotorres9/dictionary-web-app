bf93f8263589e64e6dfce39fd38a57e740debdd6
Diego Torres <diegoetorres1@gmail.com>
2024-12-07 21:41:32 -0500

    working to render part of speech and definitions of search word

--- script.js
+++ script.js
@@ -10,23 +10,64 @@ let dictionaryUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
 fetch(dictionaryUrl)
     .then(response => {
         if (!response.ok) {
-            throw new Error ('Networl rresponse not ok');
+            definitionContainer.innerHTML = `
+                <div class="error-msg-container">
+                    <span>☹️</span>
+                    <span>No Definitions Found</span>
+                    <p>Sorry pal, we couldn't find definitions for the word you were looking for. You can try the search again at later time or head to the web instead.</p>
+                <div>
+            `
+            throw new Error ('Network response not ok')
+
         }
         return response.json();
     })
     .then (searchword => {
         console.log(JSON.stringify(searchword, null, 2));
-
+        // definitionContainer.innerHTML = JSON.stringify(searchword, null, 2);
+        
         searchword.map((search) => {
-            let {word, phonetic} = search;
+            let {word, phonetic, sourceUrls, meanings} = search;
 
-            definitionContainer.innerHTML = `
-            <div>
-                <h1>${word}</h1>
-                <span>${phonetic}</span>
-            </div>
-            `
+            // console.log(search.meanings);
+
+        definitionContainer.innerHTML = 
+        `
+        <div>
+            <h1>${word}</h1>
+            <span>${phonetic}</span>
+        </div>
+
+        <div>
+        ${
+            meanings.map((meaning) => {
+                let{ partOfSpeech, definitions } = meaning;
+                console.log(partOfSpeech);
+                console.log(definitions);
+            })
+        }
+            
+        </div>
+
+        <div>
+            <p>Source</p>
+            <a href="${sourceUrls[0]}">${sourceUrls[0]}</a>
+        </div>
+        `
         })
+
+
+        // searchword.map((search) => {
+        //     let {word, phonetic, sourceUrls} = search;
+
+        //     definitionContainer.innerHTML = `
+        //     <div>
+        //         <h1>${word}</h1>
+        //         <span>${phonetic}</span>
+        //         <span>Source <a href="${sourceUrls}">${sourceUrls}</a></span>
+        //     </div>
+        //     `
+        // })
     });
 });
 
